38671e84f85129db42cd4c1ce1d609e5e5bc0f31
Vivien Nicolas <vnicolas@mozilla.com>
2016-06-27 12:59:04 +0200

    Add basic support to allow ServiceWorker over http for testing

--- bootstrap.js
+++ bootstrap.js
@@ -2,6 +2,7 @@ const Cu = Components.utils;
 const Ci = Components.interfaces;
 const Cm = Components.manager;
 
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/Extension.jsm");
 
@@ -58,4 +59,53 @@ function shutdown() {
 }
 
 function uninstall() {
+}
+
+const ProgressListener = {
+  onStateChange: function(webProgress, request, stateFlags, status) {
+    if (stateFlags & Ci.nsIWebProgressListener.STATE_START) {
+      let window = webProgress.DOMWindow;
+
+      let windowUtils = window.QueryInterface(Ci.nsIInterfaceRequestor)
+                                .getInterface(Ci.nsIDOMWindowUtils);
+      windowUtils.serviceWorkersTestingEnabled = true;
+    }
+  },
+
+  onLocationChange: function(webProgress, request, location, flags) {},
+  onSecurityChange: function(webProgress, request, state) {},
+  onStatusChange: function(webProgress, request, status, message) {},
+  onProgressChange: function(webProgress, request, curSelfProgress,
+                               maxSelfProgress, curTotalProgress, maxTotalProgress) {},
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIWebProgressListener,
+                                         Ci.nsISupportsWeakReference]),
+};
+
+function allowServiceWorkerForHttp(aXULWindow) {
+  // Same pref than devtools
+  Services.prefs.setBoolPref('devtools.serviceWorkers.testing.enabled', true);
+  Services.prefs.setBoolPref('browser.cache.disk.enable', true);
+
+  let window = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                         .getInterface(Ci.nsIDOMWindow);
+  window.QueryInterface(Ci.nsIInterfaceRequestor)
+        .getInterface(Ci.nsIWebNavigation)
+        .QueryInterface(Ci.nsIDocShell)
+        .QueryInterface(Ci.nsIWebProgress)
+        .addProgressListener(
+          ProgressListener,
+          Ci.nsIWebProgress.NOTIFY_STATE_WINDOW
+        );
+}
+
+Services.wm.addListener({
+  onOpenWindow: allowServiceWorkerForHttp,
+  onCloseWindow: function(aXULWindow) {},
+  onWindowTitleChange: function(aXULWindow, aNewTitle) {}
+});
+
+let currentWindow = Services.wm.getMostRecentWindow("navigator:browser");
+if (currentWindow) {
+  allowServiceWorkerForHttp(currentWindow);
 }
